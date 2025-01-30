1618837db681da520b1d9a53f0ede3a47aa429f7
WILLIAMS OCHIENG <143290413+Savville@users.noreply.github.com>
2025-01-30 11:18:28 -0800

    Create colour_shadow.jsx

---
+++ Components/Shadow/colour_shadow.jsx
@@ -0,0 +1,10 @@
+const Blorb = () => {
+  return (
+    <div
+      className="absolute w-[758px] h-[398.71px] left-[258px] top-[344px] 
+                 bg-[rgba(255,0,84,0.8)] blur-[158.977px]"
+    ></div>
+  );
+};
+
+export default Blorb;
